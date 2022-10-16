6ec0210fed5b8a60e707353c72708080698391bc
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-10-16 18:15:42 +0700

    first-screen mobile full completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 const firstScreen = document.querySelector('.first-screen');
+const burgerBody = document.querySelector('.burger');
+const body = document.querySelector('body');
+let isBurger = false;
 
+
+
+
+
+// burger
+const burgerClick = () => {
+    isBurger = isBurger != false ? burgerFalse() : burgerTrue();
+    function burgerTrue() {
+        body.style.overflow = "hidden";
+        burgerBody.style.zIndex = 1;
+        burgerBody.style.opacity = 1;
+        return true;
+    }
+
+    function burgerFalse() {
+        body.style.overflow = "auto";
+        burgerBody.style.zIndex = "-1";
+        burgerBody.style.opacity = 0;
+        return false;
+    }
+}
+
+
+// scroll
 function scrollDown() {
     window.scrollBy({
         top: firstScreen.offsetHeight,
